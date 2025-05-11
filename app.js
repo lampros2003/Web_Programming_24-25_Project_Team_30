025d52e564c6950bb7778e1a95fa2e4cbc673b22
lampros2003 <46668710+lampros2003@users.noreply.github.com>
2025-05-11 16:27:22 +0300

    Made index screen
    index screen,. What the user views when entering app

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +20,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Add route aliases for clarity
+app.use('/about', indexRouter);
+app.use('/menu', indexRouter);
+app.use('/reservations', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'RESTAURANT',
+    activeTab: 'about' // Default active tab
+  });
+});
+
+/* GET about us page */
+router.get('/about', function(req, res, next) {
+  res.render('index', { 
+    title: 'RESTAURANT', 
+    activeTab: 'about'
+  });
+});
+
+/* GET menu page */
+router.get('/menu', function(req, res, next) {
+  res.render('index', { 
+    title: 'RESTAURANT', 
+    activeTab: 'menu'
+  });
+});
+
+/* GET reservations page */
+router.get('/reservations', function(req, res, next) {
+  res.render('index', { 
+    title: 'RESTAURANT', 
+    activeTab: 'reservations'
+  });
 });
 
 module.exports = router;
